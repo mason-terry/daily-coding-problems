7b140bf07ee0d2a92e7a40213eca554ac05ba0b0
Mason Terry <mason.terry@q2ebanking.com>
2020-08-13 21:57:13 -0500

    added first draft solution

--- javascript/081320.js
+++ javascript/081320.js
@@ -8,4 +8,25 @@
  * Bonus: Can you do this in one pass?
  */
 
+const checkForNum = (arr, k) => {
+  let tmp = null
+  let check = 0
+  for (let i = 0; i <  arr.length; i++) {
+    tmp = arr[i]
+    check = k - tmp
+    arr[i] = null
+    if (arr.includes(check)) {
+      return true
+    }
+    arr[i] = tmp
+  }
+  return false
+}
 
+console.log(checkForNum([10, 15, 3, 7], 25))
+console.log(checkForNum([10, 15, 3, 7], 13))
+console.log(checkForNum([10, 15, 3, 7], 17))
+console.log(checkForNum([10, 15, 3, 7], 18))
+console.log(checkForNum([10, 15, 3, 7], 22))
+console.log(checkForNum([10, 15, 3, 7], 10))
+console.log(checkForNum([10, 15, 3, 7], 42))
