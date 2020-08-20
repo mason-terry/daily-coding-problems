c7c3f6cb3521decab37d173585ecb4a49c3f5483
Mason Terry <mason.terry@q2ebanking.com>
2020-08-20 11:37:56 -0500

    add 8/19 and 8/20 questions

---
+++ javascript/082020.js
@@ -0,0 +1,19 @@
+/*
+ * A universal tree (which stands for "universal value") is a tree
+ * where all nodes under it have the smae value.
+ *
+ * Given the root to a binary tree, count the number of universal
+ * subtrees.
+ *
+ * For example, the following tree has 5 subtrees:
+ *
+ *        0
+ *       / \
+ *      1   0
+ *         / \
+ *        1   0
+ *       / \
+ *      1   1
+ */
+
+
