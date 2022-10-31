c970837f42be37e83cdbe5b0b47fba9006abeac5
Mason <masonjterry@aol.com>
2022-10-31 16:22:59 -0500

    finish 102022.js

--- javascript/102022.js
+++ javascript/102022.js
@@ -8,21 +8,34 @@ For example, if the list is [-10, -10, 5, 2], we should return 500, since that's
 You can assume the list has at least three integers.
  * */
 
-const largestProduct = (arr, int) => {
-  console.log(int);
-  const largestNums = [];
+const largestProduct = (arr) => {
+  const sArr = sort(arr);
+  const checkOne = sArr[0] * sArr[1] * sArr[sArr.length - 1];
+  const checkTwo =
+    sArr[sArr.length - 1] * sArr[sArr.length - 2] * sArr[sArr.length - 3];
+
+  if (checkOne > checkTwo) return checkOne;
+  return checkTwo;
+};
+
+const sort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    console.log();
-    if (i === 0) {
-      largestNums.push(arr[i]);
+    if (arr[i] > arr[i + 1]) {
+      let tmp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = tmp;
+      sort(arr);
     }
   }
   return arr;
 };
 
 const testOne = [-10, -10, 5, 2];
-const int = 3;
+const testTwo = [-10, 10, 5, 2];
+const testThree = [-1, -1, 5, 2];
+const testFour = [100, 100, 100, 100];
 
-console.log(
-  `TEST ONE: receieved: ${largestProduct(testOne, int)} expected: 500`
-);
+console.log(`TEST ONE: ${largestProduct(testOne)} expected: 500`);
+console.log(`TEST TWO: ${largestProduct(testTwo)} expected: 100`);
+console.log(`TEST THREE: ${largestProduct(testThree)} expected: 5`);
+console.log(`TEST FOUR: ${largestProduct(testFour)} expected: 10000`);
