fcb5f646d068fe8985b3515738c8eb9fcbe4e435
Mason <masonjterry@aol.com>
2022-11-02 12:05:25 -0500

    completed 103022.js

--- javascript/103022.js
+++ javascript/103022.js
@@ -7,7 +7,28 @@ Write a function to simulate an unbiased coin toss.
 */
 
 const simulateCoinToss = () => {
-  return true;
+  const randNum = Math.floor(Math.random() * 10);
+  if (randNum >= 2) {
+    return "heads";
+  }
+  return "tails";
 };
 
-console.log(`TEST ONE: ${simulateCoinToss()}`);
+const test100Flips = () => {
+  let heads = 0;
+  let tails = 0;
+  for (let i = 0; i < 100; i++) {
+    const toss = simulateCoinToss();
+    if (toss === "heads") {
+      heads++;
+    } else {
+      tails++;
+    }
+  }
+
+  console.log({ heads });
+  console.log({ tails });
+  return { heads, tails };
+};
+
+test100Flips();
