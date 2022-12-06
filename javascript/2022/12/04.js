9b87fa6029dd8d518bfc9dceaedf2cf0205eec34
Mason <masonjterry@gmail.com>
2022-12-06 11:36:32 -0600

    new file format

---
+++ javascript/2022/12/04.js
@@ -0,0 +1,11 @@
+/*
+This problem was asked by Twitter.
+
+Given a list of numbers, create an algorithm that arranges them in order to form the largest possible integer. For example, given [10, 7, 76, 415], you should return 77641510.
+*/
+
+const solution = () => {
+  return true;
+};
+
+console.log(`test one: ${solution()}`);
